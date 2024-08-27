937514ae86f223d0f0b999431c6fd68ead9fd8aa
Ellis Guillen <116373777+710619EG@users.noreply.github.com>
2024-08-27 09:46:35 -0700

    Don't have time to type this

--- P7QuickTaskGuillen/Assets/Scripts/PlayerController.cs
+++ P7QuickTaskGuillen/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float canWeGetMuchHigher;
+    private float horizontal;
+    public float speed = 1;
+    public GameOverScreen GameOverScreen;
+    bool crown = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, canWeGetMuchHigher));
+        }
+        transform.Translate (new Vector2(horizontal * speed * Time.deltaTime, 0f));
     }
 }
